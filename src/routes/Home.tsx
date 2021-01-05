5c4e381e56dc082121e0ab2adcd0753b2d33e43f
Jaewon <sorhd134@gmail.com>
2021-01-05 20:38:31 +0900

    Add Nweet Form

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "../base";
 
 const Home = () => {
-  return <>Home</>;
+  const [nweet, setNweet] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await dbService.collection("nweets").add({
+      nweet,
+      createdAt: Date.now(),
+    });
+    setNweet("");
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = e;
+    setNweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+          value={nweet}
+          onChange={handleChange}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
